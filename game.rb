69d2ed1f2671cac0d2fe7a18cb9087bb978809ac
thracken <jeffgreenland.1@gmail.com>
2016-06-29 19:56:13 -0700

    Finished game. Went way faster than I thought it would...

--- game.rb
+++ game.rb
@@ -4,10 +4,22 @@ require 'sinatra/reloader'
 enable :sessions
 
 get '/' do
-  if session[:game_over] || session[:answer].nil?
+  if session[:answer].nil?
     redirect to('/new')
+  elsif session[:game_over]
+    redirect to('/game_over')
+  elsif session[:hidden] == session[:answer]
+    redirect to ('/winner')
   end
   erb :index
+end
+
+get '/game_over' do
+  erb :game_over
+end
+
+get '/winner' do
+  erb :winner
 end
 
 get '/new' do
@@ -18,11 +30,13 @@ get '/new' do
   session[:wrong_guesses] = 0
   session[:wrong_letters] = []
   session[:game_over] = false
+  session[:message] = ""
   redirect to('/')
 end
 
 post '/' do
-
+  compare(params["guess"])
+  redirect to('/')
 end
 
 helpers do
@@ -44,4 +58,35 @@ helpers do
     return hidden
   end
 
+  def compare(letter)
+    message = ""
+    if session[:guesses].include?(letter)
+      session[:message] = "You've already guessed that, try again."
+      return
+    elsif letter.length > 1
+      session[:message] = "You can only guess one letter at a time. Try again."
+      return
+    elsif letter.to_i.to_s == letter
+      session[:message] = "Sorry, only letters work. Try again."
+      return
+    elsif session[:answer].include?(letter)
+      session[:guesses] << letter
+      session[:answer].length.times do |x|
+        if session[:answer][x] == letter
+          session[:hidden][x] = letter
+        end
+      end
+      session[:message] = "You guessed correctly!"
+    else
+      session[:wrong_guesses] += 1
+      session[:wrong_letters] << letter
+      session[:guesses] << letter
+      session[:message] = "Your guess was wrong!" if session[:wrong_guesses] < 6
+      if session[:wrong_guesses] == 6
+        session[:message] = "Nope, sorry! That was your last guess. Better luck next time!<br /><h2>The answer was: #{session[:answer]}</h2>"
+        session[:game_over] = true
+      end
+    end
+    return session[:hidden]
+  end
 end
